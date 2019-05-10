9f393ea6e252e6d4a4885ca934571644e340818f
Celeste Glavin <cmugla@gmail.com>
2019-05-09 21:32:40 -0400

    remove the honeypot bot field

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -45,7 +45,6 @@ export default class Index extends React.Component {
                 method="post"
                 action="/contact/thanks/"
                 data-netlify="true"
-                data-netlify-honeypot="bot-field"
                 onSubmit={this.handleSubmit}
               >
                 {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
