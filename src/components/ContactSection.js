c77f59f7b21a34550f7281534aef475d433522bf
Celeste Glavin <cmugla@gmail.com>
2019-06-01 18:43:41 -0400

    fix contact section so it looks like other pages

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,75 +1,79 @@
 import React from 'react'
 
 export default props => (
-  <section className="section">
+  <section className="section section--gradient">
     <div className="container">
-      <div className="content">
-        <h1>Contact</h1>
-        <form
-          name="contact"
-          method="post"
-          action="/contact/thanks/"
-          data-netlify="true"
-          onSubmit={props.handleSubmit}
-        >
-          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type="hidden" name="form-name" value="contact" />
-          <div hidden>
-            <label>
-              Don’t fill this out:{' '}
-              <input name="bot-field" onChange={props.handleChange} />
-            </label>
+      <div className="columns">
+        <div className="column is-10 is-offset-1">
+          <div className="section">
+            <h2 className="title is-size-3 has-text-weight-bold is-bold-light">Contact</h2>
+            <form
+              name="contact"
+              method="post"
+              action="/contact/thanks/"
+              data-netlify="true"
+              onSubmit={props.handleSubmit}
+            >
+              {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+              <input type="hidden" name="form-name" value="contact" />
+              <div hidden>
+                <label>
+                  Don’t fill this out:{' '}
+                  <input name="bot-field" onChange={props.handleChange} />
+                </label>
+              </div>
+              <div className="field">
+                <label className="label" htmlFor={'name'}>
+                  Your name
+                </label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type={'text'}
+                    name={'name'}
+                    onChange={props.handleChange}
+                    id={'name'}
+                    required={true}
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label" htmlFor={'email'}>
+                  Email
+                </label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type={'email'}
+                    name={'email'}
+                    onChange={props.handleChange}
+                    id={'email'}
+                    required={true}
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label" htmlFor={'message'}>
+                  Message
+                </label>
+                <div className="control">
+                  <textarea
+                    className="textarea"
+                    name={'message'}
+                    onChange={props.handleChange}
+                    id={'message'}
+                    required={true}
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <button className="btn is-link" type="submit">
+                  Send
+                </button>
+              </div>
+            </form>
           </div>
-          <div className="field">
-            <label className="label" htmlFor={'name'}>
-              Your name
-            </label>
-            <div className="control">
-              <input
-                className="input"
-                type={'text'}
-                name={'name'}
-                onChange={props.handleChange}
-                id={'name'}
-                required={true}
-              />
-            </div>
-          </div>
-          <div className="field">
-            <label className="label" htmlFor={'email'}>
-              Email
-            </label>
-            <div className="control">
-              <input
-                className="input"
-                type={'email'}
-                name={'email'}
-                onChange={props.handleChange}
-                id={'email'}
-                required={true}
-              />
-            </div>
-          </div>
-          <div className="field">
-            <label className="label" htmlFor={'message'}>
-              Message
-            </label>
-            <div className="control">
-              <textarea
-                className="textarea"
-                name={'message'}
-                onChange={props.handleChange}
-                id={'message'}
-                required={true}
-              />
-            </div>
-          </div>
-          <div className="field">
-            <button className="btn is-link" type="submit">
-              Send
-            </button>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   </section>
